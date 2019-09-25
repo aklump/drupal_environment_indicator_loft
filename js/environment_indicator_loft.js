702a2d078e8d75fbcddf41a491ddb2f77e4435a6
Aaron Klump <sourcecode@intheloftstudios.com>
2019-09-24 19:30:45 -0700

    Add JS click support.

---
+++ js/environment_indicator_loft.js
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * Provide click handling for the environment_indicator.
+ */
+
+(function($, Drupal) {
+  'use strict'
+
+  Drupal.behaviors.environmentIndicatorLoft = {
+    attach: function(context) {
+      var $indicator = $('.environment-indicator')
+
+      // Hide a previously hidden indicator.
+      var isHiddenByCookie = $.cookie('environment-indicator')
+      if (isHiddenByCookie) {
+        $indicator.hide()
+        return
+      }
+
+      // Setup for toggle interaction.
+      var $toggle = $('.js-environment-indicator__hide')
+
+      // Single click hides until next page load.
+      $toggle.once().click(function(e) {
+        // Was the meta key held down? Set cookie?
+        if (e.metaKey) {
+          $indicator.fadeOut(function() {
+            $indicator.fadeIn(function() {
+              $indicator.fadeOut()
+            })
+          })
+
+          // Cookie handling.
+          var expiry = new Date(),
+            time = expiry.getTime() + 600 * 1000
+          expiry.setTime(time)
+          $.cookie('environment-indicator', 'hidden', {
+            expires: expiry,
+          })
+        } else {
+          $indicator.fadeOut()
+        }
+        return false
+      })
+    },
+  }
+})(jQuery, Drupal)
