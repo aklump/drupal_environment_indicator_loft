d5d33a0b0274503f6c9149682becb8463d19477d
Aaron Klump <sourcecode@intheloftstudios.com>
2019-09-26 14:08:38 -0700

    fix the double click, which was working as meta-click

--- documentation/source/_overview.md
+++ documentation/source/_overview.md
@@ -20,8 +20,8 @@ This is an alternative UX by [In the Loft Studios](http://intheloftstudios.com) 
 ## Contributing
 
 If you find this project useful... please consider [making a donation](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4E5KZHDQCEUV8&item_name=Gratitude%20for%20aklump%2Fenvironment_indicator_loft).
-
 ## Usage
 
 * Single click the title in the bottom right hand and the indicator will be hidden for that request.
 * Double click the title and a cookie will be set for 30 minutes to hide the indicator.  You can use this if you are doing some critical theming and you do not want the visual interference of the border for a time.
+* Hold down META while clicking to access the switcher (@todo This has not been built yet.)
--- js/environment_indicator_loft.js
+++ js/environment_indicator_loft.js
@@ -4,44 +4,60 @@
  */
 
 (function($, Drupal) {
-  'use strict';
+  'use strict'
 
   Drupal.behaviors.environmentIndicatorLoft = {
     attach: function(context) {
-      var $indicator = $('.environment-indicator');
+
+      function hideForLonger() {
+        $indicator.fadeOut(function() {
+          $indicator.fadeIn(function() {
+            hideForRequest()
+          })
+        })
+
+        // Cookie handling.
+        var expiry = new Date(),
+          time = expiry.getTime() + 600 * 1000
+        expiry.setTime(time)
+        $.cookie('environment-indicator', 'hidden', {
+          expires: expiry,
+        })
+      }
+
+      /**
+       * Hide indicator for the duration of this request.
+       */
+      function hideForRequest() {
+        $indicator.fadeOut()
+      }
+
+      var $indicator = $('.environment-indicator')
 
       // Hide a previously hidden indicator.
-      var isHiddenByCookie = $.cookie('environment-indicator');
+      var isHiddenByCookie = $.cookie('environment-indicator')
       if (isHiddenByCookie) {
-        $indicator.hide();
-        return;
+        $indicator.hide()
+        return
       }
 
       // Setup for toggle interaction.
-      var $toggle = $('.js-environment-indicator__hide');
+      var $toggle = $('.js-environment-indicator__hide')
 
       // Single click hides until next page load.
+      $toggle.once().dblclick(function(e) {
+        hideForLonger()
+      })
+
       $toggle.once().click(function(e) {
         // Was the meta key held down? Set cookie?
         if (e.metaKey) {
-          $indicator.fadeOut(function() {
-            $indicator.fadeIn(function() {
-              $indicator.fadeOut();
-            });
-          });
-
-          // Cookie handling.
-          var expiry = new Date(),
-            time = expiry.getTime() + 600 * 1000;
-          expiry.setTime(time);
-          $.cookie('environment-indicator', 'hidden', {
-            expires: expiry,
-          });
+          // TODO Handle the switcher.
         } else {
-          $indicator.fadeOut();
+          hideForRequest()
         }
-        return false;
-      });
+        return false
+      })
     },
-  };
-})(jQuery, Drupal);
+  }
+})(jQuery, Drupal)
