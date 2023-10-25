3fbcddb8b06c275252051488fd76ca94a001063e
Aaron Klump <sourcecode@intheloftstudios.com>
2023-10-24 19:28:29 -0700

    Update WP config.

--- .web_package/hooks/build/10_scss.sh
+++ .web_package/hooks/build/10_scss.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-# https://www.npmjs.com/package/node-sass-chokidar
-sass=./node_modules/.bin/node-sass-chokidar
-
-# config
-style=compressed
-# endconfig
-
-test -e "$7/css" && rm -r "$7/css"
-$sass --output-style=${style} "$7/environment_indicator.scss" -o "$7/css"
+test -e "./css" && rm -r "./css"
+yarn
+./node_modules/.bin/sass --style=compressed "./environment_indicator.scss" "./css/environment_indicator.css"
